b0bf7949b2a70d5ffac968419db120e3b385c6e3
SUGANTH7V <106509960+SUGANTH7V@users.noreply.github.com>
2023-04-25 10:34:32 +0530

    Commit 03

---
+++ Welcome-1/src/main/java/com/example/Welcomedemo/controller/Welcome1Application.java
@@ -0,0 +1,18 @@
+package com.example.Welcomedemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Welcome1Application {
+	
+	@ResponseBody
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String Welcome() {
+		return "Welcome";
+		
+	}
+
+}
---
+++ Welcome2/src/main/java/com/example/Welcomedemo/Welcome2Application.java
@@ -0,0 +1,18 @@
+package com.example.Welcomedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Welcome2Application {
+	
+	@ResponseBody
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String Welcome() {
+		return "Welcome";
+		
+	}
+
+}
