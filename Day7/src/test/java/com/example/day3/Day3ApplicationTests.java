fa752a639112605a7d34a402870d676d64bfa6a5
SUGANTH7V <106509960+SUGANTH7V@users.noreply.github.com>
2023-05-06 10:32:58 +0530

    Update Day7ApplicationTests.java

--- Day7/src/test/java/com/example/day3/Day3ApplicationTests.java
+++ Day7/src/test/java/com/example/day3/Day3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Day3ApplicationTests {
+class Day7ApplicationTests {
 
 	@Test
 	void contextLoads() {
